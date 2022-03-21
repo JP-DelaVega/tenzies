097e22e1940c370b9e1484f9b4ac9e172ffa39d9
John Philip Dela Vega <94108212+JP-DelaVega@users.noreply.github.com>
2022-03-21 08:15:20 +0800

    Update Records.js

--- src/components/Records.js
+++ src/components/Records.js
@@ -5,7 +5,7 @@ export default function Records(props) {
   
   return (
     
-    <div >
+    <div className='top-10' >
         {props.top > 9 ? props.top : "0" + props.top }&emsp;&emsp;
         {props.rollCount > 9 ? props.rollCount : "0" +props.rollCount }&emsp;&emsp;
         {props.totalTime}
